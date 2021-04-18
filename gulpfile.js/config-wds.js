fd24003ef26a5014d881d021d30bfdf109d34287
Игорь (IPcorp) Патиевич <ipcorps@gmail.com>
2021-04-19 01:28:26 +0300

    add ts from/to

--- gulpfile.js/config-wds.js
+++ gulpfile.js/config-wds.js
@@ -4,9 +4,11 @@
 module.exports = {
     ts: {
         use: true,          // TypeScript -> JavaScript
+        dirFrom: "",        // Replacing the path from
+        dirTo: "",          // Replacing the path to
         mini: true,         // Using minification
         extjs: ".m.js",     // Output file extension
-        middle: true        // Saving an intermediate file during minification.
+        middle: true        // Saving an intermediate non-minified file during minification.
     },
     pug: true,              // Pug -> HTML
     styl: true              // Stylus -> CSS
--- gulpfile.js/ts.js
+++ gulpfile.js/ts.js
@@ -16,13 +16,15 @@ let tsProject = ts.createProject(tsOpt);
 exports.change = path => {
 
     // TypeScript processing
-    src(path)                                                                       // Reading the file 
-        .pipe(tsProject())                                                          // TypeScript -> JavaScript
-        .on('error', console.log)                                                   // For oops caught a mistake 🙀
-        .pipe(gulpif(wdsOpt.middle, dest('.')))                                     // Saving an intermediate file
-        .pipe(gulpif(wdsOpt.mini, terser(tersOpt)))                                 // Javascript minifier and ... what else you want
-        .pipe(gulpif(wdsOpt.extjs, gulprename({ extname: wdsOpt.extjs })))          // Output file extension
-        .pipe(dest('.'));                                                           // Saving the file
+    src(path)                                                                               // Reading the file 
+        .pipe(tsProject())                                                                  // TypeScript -> JavaScript
+        .on('error', console.log)                                                           // For oops caught a mistake 🙀
+        .pipe(gulpif(wdsOpt.middle, dest('.')))                                             // Saving an intermediate file
+        .pipe(gulpif(wdsOpt.mini, terser(tersOpt)))                                         // Javascript minifier and ... what else you want
+        .pipe(gulpif(wdsOpt.extjs, gulprename({ extname: wdsOpt.extjs })))                  // Output file extension
+        .pipe(gulpif(!!wdsOpt.dirFrom, gulprename(                                          // Checking and setting the path
+            dir => dir.dirname = dir.dirname.replace(wdsOpt.dirFrom, wdsOpt.dirTo))))
+        .pipe(dest('.'));                                                                   // Saving the file
 
     // To see something happen
     console.log('\x1b[36m%s\x1b[0m', path, 'processed');
